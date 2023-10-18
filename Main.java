1e706702c62ea9453c4d43ada7ce69a77f7216b6
Salian17 <sazonov1744@yandex.ru>
2023-10-18 14:49:31 +0300

    edit file Main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
         System.out.println("Небо голубое");
+        System.out.println("Трава зелёная");
     }
 }
