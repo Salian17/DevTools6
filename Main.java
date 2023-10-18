8a86e78f8a79c26eaca01d595b06bff81a108d6b
Salian17 <sazonov1744@yandex.ru>
2023-10-18 14:30:50 +0300

    edit file Main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Добавлено в ветке main");
         System.out.println("Виталя слабый");
+        System.out.println("Вадим бездарь");
     }
 }
